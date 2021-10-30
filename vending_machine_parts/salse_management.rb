bcb8ceb39a35d5f733d562abf760df030d096041
yusei masuda <masuda.no@icloud.com>
2021-10-30 22:04:08 +0900

    salse_management作成しました

--- vending_machine_parts/salse_management.rb
+++ vending_machine_parts/salse_management.rb
@@ -1,10 +1,12 @@
 class SalesManagement
-    attr_reader :proceeds
-    def initialize
-      @proceeds = 0
-    end
-    def proceeds_increase(value)
-    end
-    def proceeds_decrease(value)
-    end
+  attr_reader :proceeds
+  def initialize
+    @proceeds = 0
   end
+  def proceeds_increase(value)
+    @proceeds += value
+  end
+  def proceeds_decrease(value)
+    @slot_money -= value
+  end
+end
