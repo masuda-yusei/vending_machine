bae49f9fd0e9cf93e384cf0cc51c54c9a4e9a6c0
yusei masuda <masuda.no@icloud.com>
2021-11-05 13:29:11 +0900

    insertwo

--- vending_machine_parts/insert.rb
+++ vending_machine_parts/insert.rb
@@ -1,15 +1,16 @@
 class Insert
+  attr_reader :slot_money
   MONEY = [10, 50, 100, 500, 1000].freeze
   def initialize
     @slot_money = 0
     # @salse_management = SalseManagement.new
   end
   def money_judgement(value) 
-    if MONEY.include?(value)
-      @slot_money += value
-    else
-      return value
-    end
+    MONEY.include?(value)
+    #   @slot_money += value
+    # else
+    #   return value
+    # end
   end
   def money_increase(value)
     @slot_money += value
@@ -17,11 +18,11 @@ class Insert
   def money_decrease(value)
     @slot_money -= value
   end
-  def money_amount
-    @slot_money += value(money_increase - money_decrease)
-    # @salse_management.proceeds
-  end
-  def slot_money
-    print @slot_money
-  end
+  # def money_amount
+  #   @slot_money += value(money_increase - money_decrease)
+  #   # @salse_management.proceeds
+  # end
+  # def slot_money
+  #   print @slot_money
+  # end
 end
